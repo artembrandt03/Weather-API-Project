44cb20d4f7858d0055fe8f2a18e94847bc403c98
Artem <artem.brandt03@gmail.com>
2026-01-08 19:35:38 -0500

    Implement settings.js

---
+++ js/settings.js
@@ -0,0 +1,8 @@
+export const OPENWEATHER = {
+  API_KEY: "a084afb880ee32a5edd1f27c4bdaccf8",
+  GEO_DIRECT_URL: "https://api.openweathermap.org/geo/1.0/direct"
+};
+
+export const LIMITS = {
+  CITY_SUGGESTIONS: 3
+};
